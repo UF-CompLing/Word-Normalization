0a6c471ace35d1765b0ccb3e678cb3946562b91e
Danny Sepler <dsepler@ufl.edu>
2016-02-18 22:04:51 -0500

    made word frequencies

--- Main.py
+++ Main.py
@@ -1,40 +1,107 @@
 import re
 from nltk.stem import PorterStemmer, LancasterStemmer, SnowballStemmer, WordNetLemmatizer
+from collections import Counter
 
-# all the stemmers
+
+# All the stemmers
+
+# it may be best just to use one at a time. for this
+# tutorial, i will use Porter Stemmer! It is the one
+# jurafsky discussed in our last Coursera video!
+
 porter = PorterStemmer()
-lancaster = LancasterStemmer()
-snowball = SnowballStemmer("english")
+	# further reading: http://tartarus.org/martin/PorterStemmer/
 
+# lancaster = LancasterStemmer()
+	# further reading: http://textminingonline.com/tag/lancaster-stemmer
+	
+# snowball = SnowballStemmer("english")
+	# further reading: http://snowball.tartarus.org/texts/introduction.html
+
+# the lemmatizer
 lemmatiser = WordNetLemmatizer()
 
-out = open('output.txt', 'w+')
-with open("first-paragraph.txt", "r+") as file:
-   for line in file:
+def Normalize(word):
+	## NOTE: the lemmatizers and stemmers have serious
+	## performance tradeoffs. keep that in mind
+	
+	# lemmatize
+	newWord = lemmatiser.lemmatize(word)
 
-       # regex expressions
-       regexed = re.compile(r'\W+', re.UNICODE).split(line)
+	# stem
+	newWord = porter.stem(newWord)
 
-       # build word to print back to file
-       toOutput = ''
-       for word in regexed:
+	# for reference, here are how you would use the
+	# other two stemmers....
+	# newWord = lancaster.stem(newWord)
+	# newWord = snowball.stem(newWord)
 
-           # lemmatize
-           newWord = lemmatiser.lemmatize(word)
-           print(newWord)
+	# make lower case, and return
+	return newWord.lower()
 
-           # stem
-           newWord = porter.stem(toOutput)
-           newWord = lancaster.stem(newWord)
-           newWord = snowball.stem(newWord)
 
-           # print(newWord + " ")
+## ~~~~~~~~~~~~~~~~~~~~
+## START OF FUNCTIONS
 
-           # add it to a new line
-           toOutput += newWord + ' '
+# input/output
+print('\nopening input file')
+text_out = open('output.txt', 'w+')
+frequencyOut = open('frequencyOutput.txt', 'w+')
 
-       # make lower case
-       toOutput = toOutput.lower()
+print('opening output file\n')
+input_file = open('Harry-Potter.txt', 'r+')
+	# the second parameter of both of these open functions is
+	# the permission.
+	#
+	#	- 'w+' means you can write permissions, and it can 
+	#					create a new file of this name if 
+	#					this file doesn't exit	
+	#
+	#	- 'r'  means read-only
 
-       # print word to output file
-       out.write(toOutput + '\n')
+print('tokenizing and normalizing.')
+print('this might take a little bit...')
+
+# store every word in here
+AllTheWords = []
+
+lineCounter = 0
+for line in input_file:
+
+	# keep track of prograss
+	print("line number " + str(lineCounter))
+	lineCounter += 1
+
+   	# regex expressions
+   	regexed = re.compile(r'\W+', re.UNICODE).split(line)
+   		# this function takes out characters that are not
+   		# letter or numbers
+
+   	# build word to print back to file
+   	toOutput = ''
+
+   	for word in regexed:
+   		
+   		# there are lots of blank spaces that get
+   		# caught up in our program
+   		if word is '':
+   			continue
+
+   		word = Normalize(word)
+   		AllTheWords.append(word)
+
+		toOutput += Normalize(word) + ' '
+
+	# print word to output file
+	text_out.write(toOutput + '\n')
+
+# SORTING
+print('\nnow, sorting the inputs')
+
+# group them
+sortedWords = Counter(AllTheWords)
+
+# print them out
+for key, value in sortedWords.most_common():
+	print(str(value) + ' ' + str(key))
+	frequencyOut.write(str(value) + ' ' + str(key) + '\n')
